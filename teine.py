41928afcb4247b48b898fb93b290dd3acc38d822
oNyXtk <assasin.outlast@gmail.com>
2025-02-13 15:23:49 +0200

    Add files via upload

---
+++ teine.py
@@ -0,0 +1,116 @@
+from pykkar import *
+
+def pykkar():
+
+ create_world("""
+ ########
+ #  >   #
+ #      #
+ #      #
+ #      #
+ #      #
+ ########
+ """)
+ step()
+ step()
+ step()
+ paint()
+ right()
+ step()
+ paint()
+ step()
+ paint()
+ step()
+ paint()
+ step()
+ paint()
+ right()
+ step()
+ paint()
+ step()
+ paint()
+ step()
+ paint()
+ step()
+ paint()
+ step()
+ paint()
+ right()
+ step()
+ paint()
+ right()
+ step()
+ paint()
+ step()
+ paint()
+ step()
+ paint()
+ step()
+ paint()
+ right()
+ right()
+ right()
+ step()
+ paint()
+ right()
+ right()
+ right()
+ step()
+ paint()
+ step()
+ paint()
+ step()
+ paint()
+ step()
+ paint()
+ right()
+ step()
+ paint()
+ right()
+ step()
+ paint()
+ step()
+ paint()
+ step()
+ paint()
+ step()
+ paint()
+ right()
+ right()
+ right()
+ step()
+ paint()
+ right()
+ right()
+ right()
+ step()
+ paint()
+ step()
+ paint()
+ step()
+ paint()
+ step()
+ paint()
+ 
+ 
+ 
+ 
+ 
+ 
+
+ 
+
+ 
+
+ 
+ 
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+
